0ae8f219b712fa0d15465b281398792324f07454
Everton Carlos De Souza Santos <evertonlensson@hotmail.com>
2024-03-30 02:22:09 -0300

    readmi

---
+++ COMANDO FOR.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <locale.h>
+
+void main ()
+{
+
+setlocale(LC_ALL,"Portuguese");
+
+// USANDO O COMANDO FOR.
+
+for (int i = 0  ; i < 10; i++) {
+  printf("%d ", i);
+}
+
+
+
+}
